68b816d93299eab2845156f6a8dc687306111bdf
nyj001012 <nyj001012@gmail.com>
2024-08-20 16:22:23 +0900

    feat: 초기 세팅

---
+++ src/main/java/nyj001012/er_bal/ErBalApplication.java
@@ -0,0 +1,13 @@
+package nyj001012.er_bal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErBalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErBalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nyj001012/er_bal/ErBalApplicationTests.java
@@ -0,0 +1,13 @@
+package nyj001012.er_bal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErBalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
