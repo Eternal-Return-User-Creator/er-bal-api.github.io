0c94f0ad83dd50b952d3409527771c3fa6ab6fd4
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-11 18:28:09 +0900

    feat(question): question의 post 시 request 객체 DTO 추가

--- src/main/java/nyj001012/er_bal/controller/QuestionController.java
+++ src/main/java/nyj001012/er_bal/controller/QuestionController.java
@@ -1,15 +1,17 @@
 package nyj001012.er_bal.controller;
 
 import nyj001012.er_bal.domain.Question;
+import nyj001012.er_bal.dto.QuestionPostRequestDTO;
 import nyj001012.er_bal.service.QuestionService;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
 @RestController
+@RequestMapping("/api")
 public class QuestionController {
 
     private final QuestionService questionService;
@@ -18,14 +20,15 @@ public class QuestionController {
         this.questionService = questionService;
     }
 
-    @PostMapping("/question")
-    public Long post(@RequestBody String questionA, @RequestBody String questionB) {
+    @PostMapping("question")
+    public ResponseEntity<Question> post(@RequestBody QuestionPostRequestDTO questionPostRequestDTO) {
         Date date = new Date();
         Question question = new Question();
-        question.setQuestionA(questionA);
-        question.setQuestionB(questionB);
+        question.setQuestionA(questionPostRequestDTO.getQuestionA());
+        question.setQuestionB(questionPostRequestDTO.getQuestionB());
         question.setCreatedDate(date);
         question.setUpdatedDate(date);
-        return questionService.post(question);
+        questionService.post(question);
+        return ResponseEntity.ok(question);
     }
 }
---
+++ src/main/java/nyj001012/er_bal/dto/QuestionPostRequestDTO.java
@@ -0,0 +1,22 @@
+package nyj001012.er_bal.dto;
+
+public class QuestionPostRequestDTO {
+    public String getQuestionB() {
+        return questionB;
+    }
+
+    public void setQuestionB(String questionB) {
+        this.questionB = questionB;
+    }
+
+    public String getQuestionA() {
+        return questionA;
+    }
+
+    public void setQuestionA(String questionA) {
+        this.questionA = questionA;
+    }
+
+    private String questionA;
+    private String questionB;
+}
