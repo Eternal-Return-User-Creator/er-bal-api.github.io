4b926dda52ba0427f548a8885b16dee34ac4f931
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-11 10:33:11 +0900

    feat(repository): findById 추가

--- src/main/java/nyj001012/er_bal/repository/IQuestionRepository.java
+++ src/main/java/nyj001012/er_bal/repository/IQuestionRepository.java
@@ -2,6 +2,9 @@ package nyj001012.er_bal.repository;
 
 import nyj001012.er_bal.domain.Question;
 
+import java.util.Optional;
+
 public interface IQuestionRepository {
     Question save(Question question);
+    Optional<Question> findById(Long id);
 }
--- src/main/java/nyj001012/er_bal/repository/QuestionRepository.java
+++ src/main/java/nyj001012/er_bal/repository/QuestionRepository.java
@@ -2,7 +2,11 @@ package nyj001012.er_bal.repository;
 
 import jakarta.persistence.EntityManager;
 import nyj001012.er_bal.domain.Question;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public class QuestionRepository implements IQuestionRepository {
     private final EntityManager entityManager;
 
@@ -14,5 +18,11 @@ public class QuestionRepository implements IQuestionRepository {
     public Question save(Question question) {
         entityManager.persist(question);
         return question;
+    }
+
+    @Override
+    public Optional<Question> findById(Long id) {
+        Question question = entityManager.find(Question.class, id);
+        return Optional.ofNullable(question);
     }
 }
