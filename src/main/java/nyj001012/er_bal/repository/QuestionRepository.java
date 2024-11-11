83594868d8b31f7cc000b5f6fe320bf3f1cfde72
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-11 10:04:30 +0900

    feat(question): 리포지토리 save 함수 구현

--- src/main/java/nyj001012/er_bal/repository/QuestionRepository.java
+++ src/main/java/nyj001012/er_bal/repository/QuestionRepository.java
@@ -1,4 +1,18 @@
 package nyj001012.er_bal.repository;
 
-public class QuestionRepository {
+import jakarta.persistence.EntityManager;
+import nyj001012.er_bal.domain.Question;
+
+public class QuestionRepository implements IQuestionRepository {
+    private final EntityManager entityManager;
+
+    public QuestionRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Question save(Question question) {
+        entityManager.persist(question);
+        return question;
+    }
 }
