4a5411d97b2ee3d210a2859b527b3db4356ac938
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-11 10:55:40 +0900

    feat(service): question.post() 구현

--- src/main/java/nyj001012/er_bal/service/QuestionService.java
+++ src/main/java/nyj001012/er_bal/service/QuestionService.java
@@ -1,4 +1,25 @@
 package nyj001012.er_bal.service;
 
+import nyj001012.er_bal.domain.Question;
+import nyj001012.er_bal.repository.QuestionRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    /**
+     * 질문 등록
+     * @param question 질문 객체
+     * @return 질문 객체의 id
+     */
+    public Long post(Question question) {
+        validateInvalidQuestion(question);
+        questionRepository.save(question);
+        return question.getId();
+    }
 }
