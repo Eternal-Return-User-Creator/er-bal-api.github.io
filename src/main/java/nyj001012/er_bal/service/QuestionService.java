03fe802a241238164f83dd2482b4a10e929ba768
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-11 11:08:16 +0900

    feat(service): 질문 길이 유효성 검사 함수 구현

--- src/main/java/nyj001012/er_bal/service/QuestionService.java
+++ src/main/java/nyj001012/er_bal/service/QuestionService.java
@@ -18,8 +18,34 @@ public class QuestionService {
      * @return 질문 객체의 id
      */
     public Long post(Question question) {
-        validateInvalidQuestion(question);
+        validateQuestion(question);
         questionRepository.save(question);
         return question.getId();
+    }
+
+    /**
+     * 질문 검증
+     * 1. 질문 길이 검증
+     * 2. 비속어 검증
+     * 3. 질문 중복 검증
+     * @param question 검증할 질문 객체
+     */
+    public void validateQuestion(Question question) {
+        validateQuestionLength(question);
+        validateQuestionProfanity(question);
+        validateQuestionDuplicate(question);
+    }
+
+    /**
+     * 질문 길이 검증 (100자 이하)
+     * @param question 중복 검증할 질문 객체
+     */
+    public void validateQuestionLength(Question question) {
+        if (question.getQuestionA().isEmpty() || question.getQuestionB().isEmpty()) {
+            throw new IllegalArgumentException("질문은 비어있을 수 없습니다.");
+        }
+        if (question.getQuestionA().length() > 100 || question.getQuestionB().length() > 100) {
+            throw new IllegalArgumentException("질문은 100자 이하이어야 합니다.");
+        }
     }
 }
