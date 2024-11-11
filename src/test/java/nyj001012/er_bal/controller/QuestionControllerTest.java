c41bb297c1a3124e3d5d7edd3948b0de098fb184
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-11 18:28:28 +0900

    test(question): controller의 질문 등록 성공 케이스 구현

---
+++ src/test/java/nyj001012/er_bal/controller/QuestionControllerTest.java
@@ -0,0 +1,65 @@
+package nyj001012.er_bal.controller;
+
+import nyj001012.er_bal.domain.Question;
+import nyj001012.er_bal.repository.QuestionRepository;
+import nyj001012.er_bal.service.QuestionService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest(QuestionController.class)
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
+public class QuestionControllerTest {
+    @MockBean
+    QuestionRepository questionRepository;
+    @MockBean
+    QuestionService questionService;
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired private WebApplicationContext context;
+
+    @BeforeEach
+    public void setUp(RestDocumentationContextProvider restDocumentationContextProvider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(restDocumentationContextProvider))
+                .alwaysDo(document("{method-name}"))
+                .build();
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void 질문_등록_성공_테스트() throws Exception {
+        String questionJson = "{\n" +
+                "  \"questionA\": \"질문A 입니다.\",\n" +
+                "  \"questionB\": \"질문B 입니다.\"\n" +
+                "}";
+        mockMvc.perform(post("/api/question")
+                        .contentType("application/json")
+                        .content(questionJson))
+                .andExpect(result -> {
+                    assertThat(result.getResponse().getStatus()).isEqualTo(200);
+                });
+    }
+}
