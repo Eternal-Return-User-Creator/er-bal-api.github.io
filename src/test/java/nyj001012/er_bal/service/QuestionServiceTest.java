94a9cebc406b95a96f123f3fb7e43608345cbb1c
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-11 12:03:22 +0900

    test(question): 질문 길이 100자 검증 테스트 추가

---
+++ src/test/java/nyj001012/er_bal/service/QuestionServiceTest.java
@@ -0,0 +1,48 @@
+package nyj001012.er_bal.service;
+
+import nyj001012.er_bal.domain.Question;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class QuestionServiceTest {
+    @Autowired QuestionService questionService;
+    private Question question = new Question();
+
+    @BeforeEach
+    public void setUp() {
+        this.question = new Question();
+        this.question.setQuestionA("질문A");
+        this.question.setQuestionB("질문B");
+        this.question.setbChoiceCount(0L);
+        this.question.setaChoiceCount(0L);
+        this.question.setCreatedDate(new Date());
+        this.question.setUpdatedDate(new Date());
+    }
+
+    @Nested
+    class 질문_길이_검증_테스트 {
+
+        @Test
+        public void 질문_길이_검증_통과() {
+            questionService.validateQuestionLength(question);
+        }
+
+        @Test
+        public void 질문_길이가_100자를_넘는_경우() {
+            // questionA가 101자인 경우
+            question.setQuestionA("a".repeat(101));
+            IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> questionService.validateQuestionLength(question));
+            assertThat(e.getMessage()).isEqualTo("질문은 100자 이하이어야 합니다.");
+        }
+    }
+}
